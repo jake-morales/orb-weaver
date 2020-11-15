4b676c266aad774c20a396ab4bec7601c9538f97
Jake Morales <jmora060@ucr.edu>
2020-11-15 00:28:31 -0800

    enabled json endpoint

---
+++ crawler.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+// Node is for the treeeeees
+type Node struct {
+	Value    string  `json:"val"`
+	Children []*Node `json:"children"`
+}
+
+func getJSONbytes(topic string, maxPages int) ([]byte, error) {
+	var js []byte
+	var err error
+	topic = strings.ToLower(topic)
+
+	// Check our cache first
+	js, err = checkCache(fmt.Sprintf("./static/cache/%s-%d.json", topic, maxPages))
+	if err == nil {
+		fmt.Println("Cache worked!")
+		return js, nil
+	}
+	fmt.Printf("Did not use cache: %s\n", err)
+
+	resp, err := http.Get(fmt.Sprintf("https://wikipedia.org/wiki/%s", topic))
+	if err != nil {
+		return js, errors.New("The server was unable to get the topic wiki page")
+	}
+	if resp.StatusCode != 200 {
+		return js, errors.New("Topic does not exist")
+	}
+
+	js, err = crawl(topic, maxPages)
+	if err != nil {
+		return js, errors.New("Crawl failed")
+	}
+
+	return js, nil
+}
+
+func checkCache(s string) ([]byte, error) {
+	js, err := ioutil.ReadFile(s)
+	if err != nil {
+		return js, errors.New("Cache miss")
+	}
+
+	return js, nil
+}
+
+func crawl(topic string, maxPages int) ([]byte, error) {
+	numPages := 0
+	numTopics := 0
+	root := Node{Value: topic}
+	queue := []*Node{}
+
+	queue = append(queue, &root)
+
+	for len(queue) > 0 && numPages < maxPages {
+		node := queue[0]
+		queue = queue[1:]
+
+		links, err := scrape(node.Value)
+		if err != nil {
+			fmt.Printf("x")
+		}
+		numPages++
+
+		for _, l := range links {
+			newNode := Node{Value: l}
+			node.Children = append(node.Children, &newNode)
+			queue = append(queue, &newNode)
+			numTopics++
+		}
+	}
+
+	// create our file to write the json to
+	w, err := os.Create(fmt.Sprintf("./static/cache/%s-%d.json", topic, maxPages))
+	defer w.Close()
+
+	// serialize the JSON to our io.Writer stream
+	err = json.NewEncoder(w).Encode(root)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b, _ := json.Marshal(root)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return b, nil
+
+	// fmt.Println("")
+	// fmt.Printf("# topics crawled: %d\n", numTopics)
+	// fmt.Printf("# pages crawled %d\n", numPages)
+	// fmt.Printf("# leftover topics: %d\n", len(queue))
+
+	// Print in console. make a better tree later?
+	// fmt.Println("")
+	// fmt.Println("")
+	// b, _ := json.MarshalIndent(root, "", "\t")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(string(b))
+}
+
+func scrape(s string) ([]string, error) {
+	links := []string{}
+
+	// Get wiki page
+	res, err := http.Get(fmt.Sprintf("https://wikipedia.org/wiki/%s", s))
+	if err != nil {
+		return links, fmt.Errorf("Error: %s", err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		return links, fmt.Errorf("Error getting webpage: " + res.Status)
+	}
+	if !strings.HasPrefix(res.Header.Get("Content-Type"), "text/html") {
+		return links, fmt.Errorf("Response content type was %s not text/html", res.Header.Get("Content-Type"))
+	}
+
+	a := res.Body
+	z := html.NewTokenizer(a)
+
+	// The See Also section is structured like this in the html
+	// <h2>
+	// 		<span class="mw-headline" id="See_also">See also</span>
+	// </h2>
+	// <ul>
+	// 		<li><a href="/wiki/Australian_native_bees" title="Australian native bees">Australian native bees</a></li>
+	// 		<li><a href="/wiki/Superorganism" title="Superorganism">Superorganism</a></li>
+	// </ul>
+
+	for {
+		tt := z.Next()
+		switch tt {
+		case html.ErrorToken:
+			return links, nil
+		case html.StartTagToken:
+			token := z.Token()
+
+			// check if it's a span html element with id="See_Also"
+			if token.Data != "span" {
+				continue
+			}
+			id := ""
+			for _, a := range token.Attr {
+				if a.Key == "id" {
+					id = a.Val
+				}
+			}
+			if id != "See_also" {
+				continue
+			}
+
+			// grab all anchors until you see end </ul>
+			for {
+				tt := z.Next()
+				switch tt {
+				case html.StartTagToken:
+					token := z.Token()
+					if token.Data == "a" {
+						for _, a := range token.Attr {
+							if a.Key == "title" && !strings.HasPrefix(a.Val, "Edit section") {
+								links = append(links, a.Val)
+								break
+							}
+						}
+					}
+				case html.EndTagToken:
+					token := z.Token()
+					if token.Data == "ul" {
+						return links, nil
+					}
+				}
+			}
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,163 +1,72 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"strconv"
+
 	"net/http"
 	"os"
-	"strings"
-
-	"github.com/xlab/treeprint"
-	"golang.org/x/net/html"
 )
 
 // "/wiki/Special:random" for a random article
 
-// Link is struct for links
-type Link struct {
-	topic string
-	node  treeprint.Tree
-}
+// TODO - add date to json name and redo after 5 days
+// TODO - compress json with gzip
+// TODO - make crawling concurrent
 
 func main() {
+
+	// Controls
+	//topicPtr := flag.String("topic", "Bees", "The root topic")
+	//maxPagesPtr := flag.Int("pages", 5, "The max number of pages to crawl")
+	//flag.Parse()
+
 	// HTTP server
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 
+	// Query Endpoint
+	http.HandleFunc("/query", queryHandler)
+
+	// Get port #
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "3000"
+		port = "80"
 	}
+
+	// Start server
 	log.Printf("Listening on :%s...", port)
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// Controls
-	maxTopics := 50
-	pagesCrawled := 0
-
-	// check root topic
-	if len(os.Args) <= 1 {
-		log.Fatal("Please run command with a topic. e.g. go run crawl_seealso.go Bees")
-	}
-	rootTopic := os.Args[1]
-	resp, err := http.Get(fmt.Sprintf("https://wikipedia.org/wiki/%s", rootTopic))
-	if err != nil {
-		log.Fatal("Couldn't fetch webpage. Are you connected to the internet?")
-	}
-	if resp.StatusCode != 200 {
-		log.Fatal("That topic does not exist")
-	}
-
-	numTopics := 0
-	root := treeprint.New()
-	queue := []Link{}
-
-	root.SetValue(rootTopic)
-	queue = append(queue, Link{topic: rootTopic, node: root})
-
-	fmt.Printf("Running")
-
-	for len(queue) > 0 && numTopics < maxTopics {
-		link := queue[0]
-		queue = queue[1:]
-
-		terms, _ := getLinks(link.topic)
-		if err != nil {
-			fmt.Printf("x")
-		} else {
-			fmt.Printf(".")
-			pagesCrawled++
-		}
-
-		for _, t := range terms {
-			newBranch := link.node.AddBranch(t)
-			queue = append(queue, Link{topic: t, node: newBranch})
-			numTopics++
-		}
-	}
-
-	fmt.Println("")
-	fmt.Println("")
-	fmt.Println(root.String())
-	fmt.Printf("# topics crawled: %d\n", numTopics)
-	fmt.Printf("# pages crawled %d\n", pagesCrawled)
-	fmt.Printf("# leftover topics: %d\n", len(queue))
 }
 
-func getLinks(s string) ([]string, error) {
-	links := []string{}
+func queryHandler(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query()
 
-	// Get wiki page
-	res, err := http.Get(fmt.Sprintf("https://wikipedia.org/wiki/%s", s))
+	topic := q.Get("topic")
+	if topic == "" {
+		http.Error(w, "no topic provided", http.StatusBadRequest)
+		return
+	}
+
+	pages := q.Get("pages")
+	if q.Get("pages") == "" {
+		http.Error(w, "no pages provided", http.StatusBadRequest)
+		return
+	}
+	maxPages, _ := strconv.Atoi(pages)
+	if maxPages > 20 {
+		http.Error(w, "too many pages", http.StatusBadRequest)
+		return
+	}
+
+	js, err := getJSONbytes(topic, maxPages)
 	if err != nil {
-		return links, fmt.Errorf("Error: %s", err)
-	}
-	defer res.Body.Close()
-	if res.StatusCode != 200 {
-		return links, fmt.Errorf("Error getting webpage: " + res.Status)
-	}
-	if !strings.HasPrefix(res.Header.Get("Content-Type"), "text/html") {
-		return links, fmt.Errorf("Response content type was %s not text/html", res.Header.Get("Content-Type"))
+		w.WriteHeader(http.StatusInternalServerError)
 	}
 
-	a := res.Body
-	z := html.NewTokenizer(a)
-
-	// The See Also section is structured like this in the html
-	// <h2>
-	// 		<span class="mw-headline" id="See_also">See also</span>
-	// </h2>
-	// <ul>
-	// 		<li><a href="/wiki/Australian_native_bees" title="Australian native bees">Australian native bees</a></li>
-	// 		<li><a href="/wiki/Superorganism" title="Superorganism">Superorganism</a></li>
-	// </ul>
-
-	for {
-		tt := z.Next()
-		switch tt {
-		case html.ErrorToken:
-			return links, nil
-		case html.StartTagToken:
-			token := z.Token()
-
-			// check if it's a span html element with id="See_Also"
-			if token.Data != "span" {
-				continue
-			}
-			id := ""
-			for _, a := range token.Attr {
-				if a.Key == "id" {
-					id = a.Val
-				}
-			}
-			if id != "See_also" {
-				continue
-			}
-
-			// grab all anchors until you see end </ul>
-			for {
-				tt := z.Next()
-				switch tt {
-				case html.StartTagToken:
-					token := z.Token()
-					if token.Data == "a" {
-						for _, a := range token.Attr {
-							if a.Key == "title" && !strings.HasPrefix(a.Val, "Edit section") {
-								links = append(links, a.Val)
-								break
-							}
-						}
-					}
-				case html.EndTagToken:
-					token := z.Token()
-					if token.Data == "ul" {
-						return links, nil
-					}
-				}
-			}
-		}
-	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
